4524f3e49eb6b8f09ed02da672082ef60c517c5b
shalom_croft <oh.shalom.0@gmail.com>
2025-08-28 13:48:27 +0000

    auto-generate provider

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -1,19 +1,14 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../models/product.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
   // initial value
   @override
   Set<Product> build() {
-    return const {
-      Product(
-        id: '4',
-        title: 'Red Backpack',
-        price: 14,
-        image: 'assets/products/backpack.png',
-      ),
-    };
+    return const {};
   }
 
   // methods to update state
@@ -30,7 +25,3 @@ class CartNotifier extends Notifier<Set<Product>> {
     }
   }
 }
-
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
