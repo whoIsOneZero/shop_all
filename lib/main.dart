85cd180058c8daf8916c76d2fb3dad891e4ae86e
shalom_croft <oh.shalom.0@gmail.com>
2025-08-28 00:05:04 +0000

    consume provider in stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shop_all/screens/home/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
+      debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
     );
   }
